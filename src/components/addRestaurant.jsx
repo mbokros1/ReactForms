f082abc3e5ce30ed50b87bac61563b1e461727ef
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-12-02 00:06:02 -0800

    Updated onchange, fixed typo in initializing useState

--- src/components/addRestaurant.jsx
+++ src/components/addRestaurant.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 function AddRestaurant({ updateRestaurants }) {
-[formData, setFormData] = useState({
+const [formData, setFormData] = useState({
   name:"",
   address:"",
   phone:"",
   cuisine:"",
   rating:"",
-  image:""});
+  image:"",});
 
 
   const handleSubmit = (e) => {
@@ -29,7 +29,67 @@ function AddRestaurant({ updateRestaurants }) {
   };
 
   return (
+     <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      
+      <div>
+        <label>image:</label>
+        <input
+          type="text"
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
+      <div>
+        <label>Phone:</label>
+        <input
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Cuisine:</label>
+        <input
+          type="text"
+          name="cuisine"
+          value={formData.cuisine}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Rating:</label>
+        <input
+          type="number"
+          name="rating"
+          min="1"
+          max="5"
+          value={formData.rating}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <button type="submit">Submit</button>
+
+      </form>
   );
 }
 
